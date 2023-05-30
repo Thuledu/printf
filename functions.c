8d2ca197389bb4317a5b043dc6ad542c95644447
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-05-30 15:02:43 -0700

    Printf project files

--- functions.c
+++ functions.c
@@ -18,7 +18,6 @@ int print_char(va_list types, char buff[],
 
 	return (handle_write_char(g, buff, flag, precisions, width, sizes));
 }
-
 /** PRINTS A STRING */
 /**
  * prints_string - Prints string
@@ -42,6 +41,7 @@ int print_string(va_list types, char buff[],
 	UNUSED(precisions);
 	UNUSED(width);
 	UNUSED(sizes);
+
 	if (string == NULL)
 	{
 		string = "(null)";
@@ -50,30 +50,29 @@ int print_string(va_list types, char buff[],
 			string = "  ";
 		}
 	}
-
 	while (string[len] != '\0')
 		len++;
-
 	if (precisions >= 0 && precisions < len)
 		len = precisions;
-
 	if (width > len)
 	{
 		if (flag & F_MINUS)
+		{
 			write(1, &string[0], len);
 			for (a = width - len; a > 0; a--)
 				write(1, " ", 1);
 			return (width);
-		else if
+		}
+		else
+		{
 			for (a = width - len; a > 0; a--)
 				write(1, " ", 1);
 			write(1, &string[0], len);
 			return (width);
+		}
 	}
 	return (write(1, string, len));
 }
-
-
 /** PRINTS THE PERCENT SIGN */
 /**
  * print_percent - Prints the percent sign
@@ -94,10 +93,8 @@ int print_percent(va_list types, char buff[],
 	UNUSED(precisions);
 	UNUSED(width);
 	UNUSED(sizes);
-
 	return (write(1, "%%", 1));
 }
-
 /** PRINTS INT */
 /**
  * print_int - Prints int
@@ -118,31 +115,23 @@ int print_int(va_list types, char buff[],
 	unsigned long int number;
 
 	d = convert_size_number(d, sizes);
-
 	if (d == 0)
 		buff[a--] = '0';
-
 	buff[BUFF_SIZE - 1] = '\0';
 	number = (unsigned long int)d;
-
 	if (d < 0)
 	{
 		number = (unsigned long int)((-1) * d);
 		negative = 1;
 	}
-
 	while (number > 0)
 	{
 		buff[a--] = (number % 10) + '0';
 		number /= 10;
 	}
-
 	a++;
-
 	return (write_number(negative, a, buff, flag, precisions, width, sizes));
 }
-
-
 /** PRINTS BINARY */
 /**
  * print_binary - Prints the unsigned number
@@ -166,9 +155,7 @@ int print_binary(va_list types, char buff[],
 	UNUSED(precisions);
 	UNUSED(width);
 	UNUSED(sizes);
-
 	d = va_arg(types, unsigned int);
-
 	e = 2147483648; /* (2 ^ 31) */
 	b[0] = d / e;
 	for (a = 1; a < 32; a++)
@@ -176,6 +163,7 @@ int print_binary(va_list types, char buff[],
 		e /= 2;
 		b[a] = (d / e) % 2;
 	}
+
 	for (a = 0, sum = 0, c = 0; a < 32; a++)
 	{
 		sum += b[a];
